64935e4ab5783013382d14ba0dbddea2ed284f2a
tauhidm01 <tauhidmahmood01@gmail.com>
2022-04-13 22:01:04 +0100

    Facebook Social Share Link Added

--- todolistprj/todolistprj/settings.py
+++ todolistprj/todolistprj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_social_share',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -60,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.request',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
