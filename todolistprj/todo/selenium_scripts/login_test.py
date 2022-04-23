1bd92ea004f0aa7e2b11cf77173a7b47e2c6469c
tauhidm01 <tauhidmahmood01@gmail.com>
2022-04-23 21:30:54 +0100

    Final

--- todolistprj/todo/selenium_scripts/login_test.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-s = Service("C:\\Users\\Tauhid\\Documents\\chromedriver.exe")
-driver = webdriver.Chrome(service=s)
